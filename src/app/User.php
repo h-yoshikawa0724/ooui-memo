330efc903b24e069df1484afa06bf3ddcfb960f4
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2021-02-04 15:16:44 +0900

    Feat: UUID導入、主キー名変更(#17)

--- src/app/User.php
+++ src/app/User.php
@@ -3,12 +3,14 @@
 namespace App;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Foundation\Auth\User as Authenticatable;
+use GoldSpecDigital\LaravelEloquentUUID\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
     use Notifiable;
+
+    protected $primaryKey = 'user_id';
 
     /**
      * The attributes that are mass assignable.
