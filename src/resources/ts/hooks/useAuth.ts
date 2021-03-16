1c07d30825debc85b9fe15faeebbce655c06cc89
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2021-03-16 15:24:37 +0900

    Refactor: ログイン処理をhooks化(#35)

---
+++ src/resources/ts/hooks/useAuth.ts
@@ -0,0 +1,90 @@
+import { useState, useCallback } from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
+import { useQueryClient, UseMutationResult, useMutation } from 'react-query';
+import axios, { AxiosError } from 'axios';
+import { User } from '../models/User';
+
+type FormData = {
+  email: string;
+  password: string;
+};
+
+const login = async (formData: FormData): Promise<User> => {
+  const { data } = await axios.post('/api/login', formData);
+  return data;
+};
+
+const useLoginMutation = (): UseMutationResult<
+  User,
+  AxiosError,
+  FormData,
+  undefined
+> => {
+  const queryClient = useQueryClient();
+
+  return useMutation(login, {
+    onSuccess: (data) => {
+      queryClient.setQueryData('user', () => data);
+    },
+  });
+};
+
+const useAuth = (): {
+  email: string;
+  password: string;
+  handleChangeEmail: (ev: React.ChangeEvent<HTMLInputElement>) => void;
+  handleChangePassword: (ev: React.ChangeEvent<HTMLInputElement>) => void;
+  handleLogin: (ev: React.FormEvent<HTMLFormElement>) => void;
+} => {
+  const history = useHistory();
+  const location = useLocation();
+  const { from } = (location.state as { from: string }) || {
+    from: { pathname: '/' },
+  };
+
+  const [email, setEmail] = useState('');
+  const [password, serPassword] = useState('');
+  const loginMutation = useLoginMutation();
+
+  const handleChangeEmail = useCallback(
+    (ev: React.ChangeEvent<HTMLInputElement>) => {
+      setEmail(ev.target.value);
+    },
+    []
+  );
+
+  const handleChangePassword = useCallback(
+    (ev: React.ChangeEvent<HTMLInputElement>) => {
+      serPassword(ev.target.value);
+    },
+    []
+  );
+
+  const handleLogin = useCallback(
+    (ev: React.FormEvent<HTMLFormElement>) => {
+      ev.preventDefault();
+      if (!email || !password) {
+        return;
+      }
+      loginMutation.mutate(
+        { email, password },
+        {
+          onSuccess: () => {
+            history.replace(from);
+          },
+        }
+      );
+    },
+    [email, password, history, from, loginMutation]
+  );
+
+  return {
+    email,
+    password,
+    handleChangeEmail,
+    handleChangePassword,
+    handleLogin,
+  };
+};
+
+export default useAuth;
