fe943a5d2c7f269a4254878f381837d24fc357f5
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2021-03-19 13:27:08 +0900

    Refactor: ログインユーザ情報取得APIのURL変更(#35)

--- src/routes/api.php
+++ src/routes/api.php
@@ -16,4 +16,4 @@ use Illuminate\Http\Request;
 Route::post('/register', 'Auth\RegisterController@register')->name('register');
 Route::post('/login', 'Auth\LoginController@login')->name('login');
 Route::post('/logout', 'Auth\LoginController@logout')->name('logout');
-Route::get('/user', 'UserController@show')->name('user');
+Route::get('/users/me', 'UserController@show')->name('user');
