d9ad591179dd5818dd9dfbf8815ac0224f3bef1a
h-yoshikawa44 <guardian.yatagaras@icloud.com>
2021-03-27 15:40:32 +0900

    Feat・Test・Docs: メモ一覧取得APIのページ当たりの件数を50に調整(#39)

--- src/tests/Feature/memos/MemoListApiTest.php
+++ src/tests/Feature/memos/MemoListApiTest.php
@@ -14,6 +14,20 @@ class MemoListApiTest extends TestCase
 
     private const DATE_TIME_FORMAT = 'Y-m-d H:i:s';
 
+    private function setExpectedData($memos)
+    {
+        return $memos->map(function ($memo) {
+            return [
+                'memo_id' => $memo->memo_id,
+                'title' => $memo->title,
+                'content' => $memo->content,
+                'created_at' => $memo->created_at->format(self::DATE_TIME_FORMAT),
+                'updated_at' => $memo->updated_at->format(self::DATE_TIME_FORMAT),
+            ];
+        })
+        ->all();
+    }
+
     public function setUp(): void
     {
         parent::setUp();
@@ -38,16 +52,7 @@ class MemoListApiTest extends TestCase
         $memos = Memo::where('user_id', $this->auth_user->user_id)->orderBy('updated_at', 'desc')->get();
 
         // data項目の期待値
-        $expected_data = $memos->map(function ($memo) {
-            return [
-                'memo_id' => $memo->memo_id,
-                'title' => $memo->title,
-                'content' => $memo->content,
-                'created_at' => $memo->created_at->format(self::DATE_TIME_FORMAT),
-                'updated_at' => $memo->updated_at->format(self::DATE_TIME_FORMAT),
-            ];
-        })
-        ->all();
+        $expected_data = $this->setExpectedData($memos);
 
         $response->assertStatus(200)
             // レスポンスJSONのdata項目に含まれる要素が5つであること
@@ -56,5 +61,55 @@ class MemoListApiTest extends TestCase
             ->assertJsonFragment([
                 "data" => $expected_data,
             ]);
+    }
+
+    /**
+     * @test
+     * メモ一覧情報取得でページネーションが機能しているか
+     */
+    public function testGetMemoListPages()
+    {
+        factory(Memo::class, 52)->create(['user_id' => $this->auth_user->user_id]);
+
+        // 1ページ目
+        $response = $this->actingAs($this->auth_user)->json('GET', route('memo.index'));
+
+        $memos = Memo::where('user_id', $this->auth_user->user_id)->orderBy('updated_at', 'desc')->limit(50)->get();
+
+        $expected_data_first = $this->setExpectedData($memos);
+
+        $response->assertStatus(200)
+            ->assertJsonCount(50, 'data')
+            ->assertJsonFragment([
+                "data" => $expected_data_first,
+            ]);
+
+        // 2ページ目
+        $response = $this->actingAs($this->auth_user)->json('GET', route('memo.index', ['page' => 2]));
+
+        $memos = Memo::where('user_id', $this->auth_user->user_id)
+            ->orderBy('updated_at', 'desc')
+            ->offset(50)
+            ->limit(50)
+            ->get();
+
+        $expected_data_second = $this->setExpectedData($memos);
+
+        $response->assertStatus(200)
+            ->assertJsonCount(2, 'data')
+            ->assertJsonFragment([
+                "data" => $expected_data_second,
+            ]);
+    }
+
+    /**
+     * @test
+     * ログインしていない時は401を返すか
+     */
+    public function testGetMemoListNotLogined()
+    {
+        $response = $this->json('GET', route('memo.index'));
+
+        $response->assertStatus(401);
     }
 }
