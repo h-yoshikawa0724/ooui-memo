f538b00eb0a4fa66459d53e01786909dc82d81c4
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2021-04-06 16:08:53 +0900

    Feat: メモ削除機能作成(#47)
    カスタムフック作成
    削除時の表示ダイアログ作成

--- src/resources/ts/components/organisms/MemoDetail.tsx
+++ src/resources/ts/components/organisms/MemoDetail.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, useState, useCallback } from 'react';
 import Alert from '@material-ui/lab/Alert';
 import AlertTitle from '@material-ui/lab/AlertTitle';
 import Box from '@material-ui/core/Box';
@@ -7,6 +7,7 @@ import { useTheme } from '@material-ui/core/styles';
 import Skeleton from '@material-ui/lab/Skeleton';
 import MemoDetailHeader from '../molecules/MemoDetailHeader';
 import MemoDetailFooter from '../molecules/MemoDetailFooter';
+import MemoDeleteDialog from '../molecules/MemoDeleteDialog';
 import { NOT_FOUND, INTERNAL_SERVER_ERROR } from '../../constants/statusCode';
 
 type Props = {
@@ -37,6 +38,15 @@ const MemoDetail: FC<Props> = ({
   handleDeleteMemo,
 }) => {
   const theme = useTheme();
+
+  // componentsには基本的にロジックを持たせないが、UIの状態に関するものなので、ここで定義している
+  const [deleteDialogOpen, setDeleteDialogOpen] = useState<boolean>(false);
+  const handleDeleteDialogOpen = useCallback(() => {
+    setDeleteDialogOpen(true);
+  }, []);
+  const handleDeleteDialogClose = useCallback(() => {
+    setDeleteDialogOpen(false);
+  }, []);
 
   if (isIdle || isLoading) {
     return (
@@ -75,49 +85,56 @@ const MemoDetail: FC<Props> = ({
   }
 
   return (
-    <Box height={1} display="flex" flexDirection="column">
-      <MemoDetailHeader
-        handleBack={handleBack}
-        handleDeleteMemo={handleDeleteMemo}
-      />
-      <Box
-        py={2}
-        style={{
-          maxHeight: 'calc(100vh - 180px)',
-          overflowY: 'scroll',
-          flexGrow: 1,
-        }}
-      >
-        <Input
-          placeholder="メモタイトル"
-          disableUnderline
-          fullWidth
-          multiline
-          value={title}
-          inputProps={{ 'aria-label': 'memo-title' }}
-          style={{ ...theme.typography.h4, ...{ whiteSpace: 'pre-wrap' } }}
-          onChange={handleChangeTitle}
+    <>
+      <Box height={1} display="flex" flexDirection="column">
+        <MemoDetailHeader
+          handleBack={handleBack}
+          handleDeleteDialogOpen={handleDeleteDialogOpen}
         />
-        <Box my={4}>
+        <Box
+          py={2}
+          style={{
+            maxHeight: 'calc(100vh - 180px)',
+            overflowY: 'scroll',
+            flexGrow: 1,
+          }}
+        >
           <Input
-            placeholder="メモ内容"
+            placeholder="メモタイトル"
             disableUnderline
             fullWidth
             multiline
-            value={content}
-            inputProps={{ 'aria-label': 'memo-content' }}
-            style={{ whiteSpace: 'pre-wrap' }}
-            onChange={handleChangeContent}
+            value={title}
+            inputProps={{ 'aria-label': 'memo-title' }}
+            style={{ ...theme.typography.h4, ...{ whiteSpace: 'pre-wrap' } }}
+            onChange={handleChangeTitle}
           />
+          <Box my={4}>
+            <Input
+              placeholder="メモ内容"
+              disableUnderline
+              fullWidth
+              multiline
+              value={content}
+              inputProps={{ 'aria-label': 'memo-content' }}
+              style={{ whiteSpace: 'pre-wrap' }}
+              onChange={handleChangeContent}
+            />
+          </Box>
         </Box>
+        <MemoDetailFooter
+          marginTop={1}
+          contentCount={content.length}
+          isUnsaved={isUnsaved}
+          updatedAt={updatedAt}
+        />
       </Box>
-      <MemoDetailFooter
-        marginTop={1}
-        contentCount={content.length}
-        isUnsaved={isUnsaved}
-        updatedAt={updatedAt}
+      <MemoDeleteDialog
+        open={deleteDialogOpen}
+        handleDeleteDialogClose={handleDeleteDialogClose}
+        handleDeleteMemo={handleDeleteMemo}
       />
-    </Box>
+    </>
   );
 };
 
