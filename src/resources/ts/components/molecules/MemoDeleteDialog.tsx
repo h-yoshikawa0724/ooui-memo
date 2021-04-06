40b878dfc8f3ae9652fafc8ec0ee10395990f716
h-yoshikawa44 <guardian.yatagaras@icloud.com>
2021-04-06 16:08:53 +0900

    Feat: メモ削除機能作成(#47)
    カスタムフック作成
    削除時の表示ダイアログ作成

---
+++ src/resources/ts/components/molecules/MemoDeleteDialog.tsx
@@ -0,0 +1,51 @@
+import React, { FC } from 'react';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
+type Props = {
+  open: boolean;
+  handleDeleteDialogClose: VoidFunction;
+  handleDeleteMemo: VoidFunction;
+};
+
+const MemoDeleteDialog: FC<Props> = ({
+  open,
+  handleDeleteDialogClose,
+  handleDeleteMemo,
+}) => (
+  <Dialog
+    open={open}
+    onClose={handleDeleteDialogClose}
+    aria-labelledby="alert-dialog-title"
+    aria-describedby="alert-dialog-description"
+  >
+    <DialogTitle id="alert-dialog-title">
+      本当にこのメモを削除してよろしいですか？
+    </DialogTitle>
+    <DialogContent>
+      <DialogContentText id="alert-dialog-description">
+        一度削除したメモは元に戻せません。
+      </DialogContentText>
+    </DialogContent>
+    <DialogActions>
+      <Button onClick={handleDeleteDialogClose} color="primary" autoFocus>
+        キャンセル
+      </Button>
+      <Button
+        onClick={() => {
+          handleDeleteMemo();
+          handleDeleteDialogClose();
+        }}
+        color="secondary"
+      >
+        削除
+      </Button>
+    </DialogActions>
+  </Dialog>
+);
+
+export default MemoDeleteDialog;
