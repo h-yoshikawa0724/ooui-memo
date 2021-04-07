f8a68da590bd3ee277347efad79c24e244549c35
h-yoshikawa44 <guardian.yatagaras@icloud.com>
2021-04-07 14:21:07 +0900

    Refactor: バックボタンの配置調整(#48)
    レスポンシブ対応部分をdisplayからHiddenコンポーネントに変えた。

--- src/resources/ts/components/atoms/BackButton.tsx
+++ src/resources/ts/components/atoms/BackButton.tsx
@@ -1,15 +1,19 @@
 import React, { FC } from 'react';
-import { IconButton } from '@material-ui/core';
+import IconButton from '@material-ui/core/IconButton';
+import Tooltip from '@material-ui/core/Tooltip';
 import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
 
 type Props = {
+  edge?: 'start' | 'end' | false;
   handleBack: VoidFunction;
 };
 
-const BackButton: FC<Props> = ({ handleBack }) => (
-  <IconButton edge="start" onClick={handleBack}>
-    <ArrowBackIosIcon />
-  </IconButton>
+const BackButton: FC<Props> = ({ edge, handleBack }) => (
+  <Tooltip title="メモ一覧へ戻る">
+    <IconButton edge={edge || false} onClick={handleBack}>
+      <ArrowBackIosIcon />
+    </IconButton>
+  </Tooltip>
 );
 
 export default BackButton;
