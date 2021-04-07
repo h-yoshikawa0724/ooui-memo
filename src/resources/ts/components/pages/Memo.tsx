f07798e6591afc16bbd5049fb132aefa92272a7b
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2021-04-07 12:51:08 +0900

    Refactor: Memoのレスポンシブ対応をHiddenコンポーネントを使うよう改善(#48)

--- src/resources/ts/components/pages/Memo.tsx
+++ src/resources/ts/components/pages/Memo.tsx
@@ -1,48 +1,36 @@
 import React, { FC } from 'react';
 import Box from '@material-ui/core/Box';
 import Container from '@material-ui/core/Container';
+import Hidden from '@material-ui/core/Hidden';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
+import { useTheme } from '@material-ui/core/styles';
 import Header from '../../containers/organisms/Header';
 import MemoList from '../../containers/organisms/MemoList';
 import MemoDetail from '../../containers/organisms/MemoDetail';
-
-const useStyles = makeStyles((theme) => ({
-  display: {
-    [theme.breakpoints.up('xs')]: {
-      display: 'none',
-    },
-    [theme.breakpoints.up('sm')]: {
-      display: 'block',
-    },
-  },
-}));
 
 type Props = {
   memoId: string;
 };
 
 const MemoBody: FC<Props> = ({ memoId }) => {
-  const classes = useStyles();
   const selectedMemo = !!memoId;
   return (
     <Box display="flex">
-      <Box
-        width={{ xs: 'calc(100vw - 32px)' }}
-        maxWidth={{ sm: 320 }}
-        pr={{ xs: 2 }}
-        flexGrow={1}
-        className={selectedMemo ? classes.display : ''}
-      >
-        <MemoList memoId={memoId} />
-      </Box>
-      <Box
-        px={{ xs: 2, sm: 4 }}
-        flexGrow={2}
-        className={!selectedMemo ? classes.display : ''}
-      >
-        <MemoDetail memoId={memoId} />
-      </Box>
+      <Hidden xsDown={selectedMemo}>
+        <Box
+          width={{ xs: 'calc(100vw - 32px)' }}
+          maxWidth={{ sm: 320 }}
+          pr={{ xs: 2 }}
+          flexGrow={1}
+        >
+          <MemoList memoId={memoId} />
+        </Box>
+      </Hidden>
+      <Hidden xsDown={!selectedMemo}>
+        <Box px={{ xs: 2, sm: 4 }} flexGrow={2}>
+          <MemoDetail memoId={memoId} />
+        </Box>
+      </Hidden>
     </Box>
   );
 };
