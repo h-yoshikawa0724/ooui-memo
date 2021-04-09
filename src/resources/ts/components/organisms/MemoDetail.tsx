95fcc256daf79f63717a197c546360f74e5ca688
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2021-04-09 13:07:49 +0900

    Feat: メモ編集のフロント側のバリデーション実装(#57)

--- src/resources/ts/components/organisms/MemoDetail.tsx
+++ src/resources/ts/components/organisms/MemoDetail.tsx
@@ -1,6 +1,8 @@
 import React, { FC, useState, useCallback } from 'react';
 import Box from '@material-ui/core/Box';
 import Input from '@material-ui/core/Input';
+import FormControl from '@material-ui/core/FormControl';
+import FormHelperText from '@material-ui/core/FormHelperText';
 import { useTheme } from '@material-ui/core/styles';
 import Skeleton from '@material-ui/lab/Skeleton';
 import GeneralAlert from '../atoms/GeneralAlert';
@@ -11,7 +13,9 @@ import { NOT_FOUND, INTERNAL_SERVER_ERROR } from '../../constants/statusCode';
 
 type Props = {
   title: string;
+  titleError: string;
   content: string;
+  contentError: string;
   isIdle: boolean;
   isLoading: boolean;
   statusCode?: number;
@@ -25,7 +29,9 @@ type Props = {
 
 const MemoDetail: FC<Props> = ({
   title,
+  titleError,
   content,
+  contentError,
   isIdle,
   isLoading,
   statusCode,
@@ -101,27 +107,37 @@ const MemoDetail: FC<Props> = ({
             flexGrow: 1,
           }}
         >
-          <Input
-            placeholder="メモタイトル"
-            disableUnderline
-            fullWidth
-            multiline
-            value={title}
-            inputProps={{ 'aria-label': 'memo-title' }}
-            style={{ ...theme.typography.h4, ...{ whiteSpace: 'pre-wrap' } }}
-            onChange={handleChangeTitle}
-          />
-          <Box my={4}>
+          <FormControl error={!!titleError}>
             <Input
-              placeholder="メモ内容"
+              placeholder="メモタイトル"
               disableUnderline
               fullWidth
               multiline
-              value={content}
-              inputProps={{ 'aria-label': 'memo-content' }}
-              style={{ whiteSpace: 'pre-wrap' }}
-              onChange={handleChangeContent}
+              value={title}
+              inputProps={{ 'aria-label': 'memo-title' }}
+              style={{ ...theme.typography.h4, ...{ whiteSpace: 'pre-wrap' } }}
+              onChange={handleChangeTitle}
             />
+            <FormHelperText id="memo-title-error-text">
+              {titleError}
+            </FormHelperText>
+          </FormControl>
+          <Box my={4}>
+            <FormControl error={!!contentError}>
+              <Input
+                placeholder="メモ内容"
+                disableUnderline
+                fullWidth
+                multiline
+                value={content}
+                inputProps={{ 'aria-label': 'memo-content' }}
+                style={{ whiteSpace: 'pre-wrap' }}
+                onChange={handleChangeContent}
+              />
+              <FormHelperText id="memo-content-error-text">
+                {contentError}
+              </FormHelperText>
+            </FormControl>
           </Box>
         </Box>
         <MemoDetailFooter
